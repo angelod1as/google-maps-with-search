c810223caaaf9187e388ca6ef0335dbb99e51c3d
Angelo Dias <oiangelodias@gmail.com>
2022-05-26 19:07:39 +0200

    Add MapComponent

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,17 +1,20 @@
 // Map.tsx
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { Wrapper, Status } from "@googlemaps/react-wrapper"
+import { MapComponent } from "./MapComponent"
 
 const render = (status: Status) => {
   switch (status) {
     case Status.LOADING:
-      return <div>Spinner</div>;
+      return <div>Spinner</div>
     case Status.FAILURE:
-      return <div>Error component</div>;
+      return <div>Error component</div>
     case Status.SUCCESS:
-      return <div>My Map</div>;
+      // These are the coordinates of our beautiful office, in Munich
+      const center = new window.google.maps.LatLng(48.136297, 11.419739)
+      return <MapComponent center={center} zoom={14} />
   }
-};
+}
 
 export const Map = () => {
-  return <Wrapper apiKey="API_KEY" render={render} />;
-};
+  return <Wrapper apiKey="API_KEY" render={render} />
+}
---
+++ src/MapComponent.tsx
@@ -0,0 +1,23 @@
+// MapComponent.tsx
+import { useEffect, useRef } from "react"
+
+export function MapComponent({
+  center,
+  zoom,
+}: {
+  center: google.maps.LatLng
+  zoom: number
+}) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (ref.current) {
+      new window.google.maps.Map(ref.current, {
+        center,
+        zoom,
+      })
+    }
+  }, [])
+
+  return <div ref={ref} />
+}
