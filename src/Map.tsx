a79956b3cdc342e080da71b11220ce1b62983ead
Angelo Dias <oiangelodias@gmail.com>
2022-05-26 19:05:24 +0200

    Add render function

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,17 @@
 // Map.tsx
-import { Wrapper } from "@googlemaps/react-wrapper";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+const render = (status: Status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <div>Spinner</div>;
+    case Status.FAILURE:
+      return <div>Error component</div>;
+    case Status.SUCCESS:
+      return <div>My Map</div>;
+  }
+};
 
 export const Map = () => {
-  return <Wrapper apiKey="API_KEY" />;
+  return <Wrapper apiKey="API_KEY" render={render} />;
 };
